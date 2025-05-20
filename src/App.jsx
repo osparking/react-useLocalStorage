e3d856787627f0af4ce1bbbde6f324647f1dfc5e
박종범 <jbpark03@gmail.com>
2025-05-20 21:44:26 +0900

    clear JWT_TOKEN shown on web page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import login from "./assets/login";
 import { useLocalStorage } from "./custom-hook";
 
 function App() {
-  const data = {username: 'admin', password: '1234', code: ''};
+  const data = { username: "admin", password: "1234", code: "" };
   const [loading, setLoading] = useState(false);
-  const [JWT_TOKEN, setValue, removeKey] = useLocalStorage('JWT_TOKEN', null);
-  const handleLogin = () => {  
+  const [JWT_TOKEN, setValue, removeKey] = useLocalStorage("JWT_TOKEN", null);
+  const handleLogin = () => {
     login(data, setLoading, setValue);
-  }
+  };
+  const handleLogout = () => {
+    removeKey();
+    setValue(null);
+  };
   const JWT_FORMATTED =
     JWT_TOKEN?.substring(0, 21) + "....." + JWT_TOKEN?.substring(120);
 
@@ -17,16 +21,11 @@ function App() {
     <>
       <h1>맞춤 후크 - 로컬 스토리지 항목</h1>
       <div className="card">
-        <p>
-          JWT_TOKEN:{" "}
-          {JWT_TOKEN 
-            ? JWT_FORMATTED 
-            : JWT_TOKEN}
-        </p>
+        <p>JWT_TOKEN: {JWT_TOKEN ? JWT_FORMATTED : JWT_TOKEN}</p>
         <button disabled={loading} onClick={handleLogin}>
           로그인
         </button>
-        <button disabled={loading} onClick={removeKey}>
+        <button disabled={loading} onClick={handleLogout}>
           로그아웃
         </button>
       </div>
