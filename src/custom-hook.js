c60dc334b6094b0479e57d548e355c27ae1482a2
박종범 <jbpark03@gmail.com>
2025-05-21 03:46:05 +0900

    comment added for useLocalStorage custom hook

--- src/custom-hook.js
+++ src/custom-hook.js
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Custom hook that saves item into the local storage
+ * 
+ * @param {*} key - local storage Item Key
+ * @param {*} initial - local storage Item' initial value
+ * @returns 
+ */
 export const useLocalStorage = (key, initial) => {
   const [value, setValue] = useState(() => {
     const savedValue = localStorage.getItem(key);
